67ea4a6a392f4aeeca4af0535ef08a305e0145f3
RJ-626 <rahuljaiswal611995@gmail.com>
2026-05-13 12:49:20 +0530

    commit-m

---
+++ chapter_03_identifier_Literals/08_Comments.js
@@ -0,0 +1,16 @@
+//This is a single comment
+
+/*
+This is a multi-line comment
+Author= "Rahul"
+Date= "2024-06-01"
+*/
+
+/**
+This is a multi-line comment
+Author= "Rahul"
+Date= "2024-06-01"
+**/
+
+
+// var g=18
